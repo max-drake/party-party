8af4ee4ee1521efb1bc2f59691854182224575dd
max <maxdrake46@gmail.com>
2023-10-06 18:54:57 +0100

    initial

--- src/client.ts
+++ src/client.ts
@@ -4,17 +4,6 @@ import PartySocket from "partysocket";
 
 declare const PARTYKIT_HOST: string;
 
-let pingInterval: ReturnType<typeof setInterval>;
-
-// Let's append all the messages we get into this DOM element
-const output = document.getElementById("app") as HTMLDivElement;
-
-// Helper function to add a new line to the DOM
-function add(text: string) {
-  output.appendChild(document.createTextNode(text));
-  output.appendChild(document.createElement("br"));
-}
-
 // A PartySocket is like a WebSocket, except it's a bit more magical.
 // It handles reconnection logic, buffering messages while it's offline, and more.
 const conn = new PartySocket({
@@ -22,19 +11,82 @@ const conn = new PartySocket({
   room: "my-new-room",
 });
 
+var color_list: {userId: string, senderColor: string}[] = [];
+
 // You can even start sending messages before the connection is open!
 conn.addEventListener("message", (event) => {
-  add(`Received -> ${event.data}`);
+  // let dataDict = JSON.parse(event.data);
+  // let id = Object.keys(dataDict)[0];
+  // let text = dataDict[id];  
+  // console.log(`connection ${id} sent message: ${text}`);
+  
+  let parsedData = JSON.parse(event.data);
+  if (parsedData.hasOwnProperty('chat_history')) {
+    let history_messages = parsedData["chat_history"];
+    if (history_messages.length > 0) {
+      console.log("chat history found")
+      history_messages.forEach((message: {senderId: string, text: string}) => {
+        chatAppend(message.text, message.senderId);
+        console.log(`${message.senderId} sent message: ${message.text}`);
+    });
+    }
+  } else if (parsedData.hasOwnProperty('new_chat')) {
+    let newMessage = parsedData["new_chat"];
+    chatAppend(newMessage.text, newMessage.senderId);
+    console.log(`${newMessage.senderId} sent message: ${newMessage.text}`);
+  } else if (parsedData.hasOwnProperty('color_list')) {
+    color_list = parsedData["color_list"];
+    console.log(`color list: ${color_list}`);
+  }
+
 });
 
-// Let's listen for when the connection opens
-// And send a ping every 2 seconds right after
-conn.addEventListener("open", () => {
-  add("Connected!");
-  add("Sending a ping every 2 seconds...");
-  // TODO: make this more interesting / nice
-  clearInterval(pingInterval);
-  pingInterval = setInterval(() => {
-    conn.send("ping");
-  }, 1000);
+conn.addEventListener("open", async () => {
+  console.log("Connected!");
 });
+
+// Get the send button and text field elements
+const sendButton = document.getElementById("send_button") as HTMLButtonElement;
+const textField = document.getElementById("text_field") as HTMLInputElement;
+const chatBox = document.getElementById("messages_box") as HTMLInputElement;
+
+// Add a keydown event listener to the text field
+textField.addEventListener("keydown", (event) => {
+  // Check if the enter key was pressed and the text field is not empty
+  if (event.key === "Enter" && textField.value.trim() !== "") {
+    // Click the send button
+    sendButton.click();
+  }
+});
+
+// Add a click event listener to the send button
+sendButton.addEventListener("click", () => {
+  // Get the text from the text field
+  const text = textField.value;
+  
+  // Send the text through conn.send
+  conn.send(text);
+
+  // Clear the text field
+  textField.value = "";
+});
+
+function chatAppend(text: string, senderId: string): void { 
+		
+  let span = document.createElement('span');
+
+  span.className = "chat_message";
+  span.style.backgroundColor = getColorByUserId(senderId)
+
+  let txt = document.createTextNode(text);
+  span.appendChild(txt);
+  chatBox.appendChild(span);
+  if (chatBox.lastChild) {
+    (chatBox.lastChild as HTMLElement).scrollIntoView(true);
+  }
+}
+
+function getColorByUserId(userId: string): string {
+  const userColor = color_list.find((element) => element.userId === userId);
+  return userColor ? userColor.senderColor : "#000000";
+}
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,110 @@
 import type * as Party from "partykit/server";
 
 export default class Server implements Party.Server {
-  constructor(readonly party: Party.Party) {}
+  constructor(public party: Party.Party) {}
 
-  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
+  messages: { senderId: string, text: string }[] = [];
+  colors: string[] = ['#ff0000',
+  '#ff2600',
+  '#ff4c00',
+  '#ff7200',
+  '#ff9900',
+  '#ffbf00',
+  '#ffe500',
+  '#f2ff00',
+  '#cbff00',
+  '#a5ff00',
+  '#7fff00',
+  '#59ff00',
+  '#33ff00',
+  '#0cff00',
+  '#00ff19',
+  '#00ff3f',
+  '#00ff66',
+  '#00ff8c',
+  '#00ffb2',
+  '#00ffd8',
+  '#00ffff',
+  '#00d8ff',
+  '#00b2ff',
+  '#008cff',
+  '#0066ff',
+  '#003fff',
+  '#0019ff',
+  '#0c00ff',
+  '#3200ff',
+  '#5900ff',
+  '#7f00ff',
+  '#a500ff',
+  '#cc00ff',
+  '#f200ff',
+  '#ff00e5',
+  '#ff00bf',
+  '#ff0098',
+  '#ff0072',
+  '#ff004c',
+  '#ff0026'];
+  assignedColors: {userId: string, senderColor: string}[] = [];
+
+  async onStart() {
+    await this.party.storage.delete("messages");
+    await this.party.storage.delete("colors");
+    await this.party.storage.delete("assignedColors");
+    this.messages = (await this.party.storage.get<{ senderId: string, text: string }[]>("messages")) ?? [];
+    this.colors = (await this.party.storage.get<string[]>("colors")) ?? this.colors;
+    this.assignedColors = (await this.party.storage.get<{userId: string, senderColor: string}[]>("assignedColors")) ?? this.assignedColors;
+  };
+
+  async onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
     // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.party.id}
-  url: ${new URL(ctx.request.url).pathname}`
+    let randomIndex = Math.floor(Math.random() * this.colors.length);
+    let new_color = this.colors[randomIndex];
+    this.colors.splice(randomIndex, 1);
+    this.assignedColors.push({userId: conn.id, senderColor: new_color});
+    this.party.storage.put("assignedColors", this.assignedColors);
+    this.party.storage.put("colors", this.colors);
+    console.log(`connection ${conn.id} assigned color: ${new_color}`);
+    console.log(`remaining number of colors: ${this.colors.length}}`);
+
+    this.party.broadcast(
+      (JSON.stringify({"color_list": this.assignedColors}))
     );
 
-    // let's send a message to the connection
-    conn.send("hello from server");
+    conn.send(JSON.stringify({"chat_history": this.messages}));
+
+  //   console.log(
+  //     `Connected:
+  // id: ${conn.id}
+  // room: ${this.party.id}
+  // url: ${new URL(ctx.request.url).pathname}`
+  //   );
+
+    
   }
 
-  onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
+  async onMessage(message: string, sender: Party.Connection) {
+    
+    // TODO
+    // do some random chance stuff and have like 20$ of messages go through normally, 70% get reversed, and 10% get turned into emojis or something
+
+    const newMessage = {
+      senderId: sender.id,
+      text: message
+    };
+  
+
+    this.messages.push(newMessage);
+    this.party.storage.put("messages", this.messages);
+    // conn.send(message);
+
     // as well as broadcast it to all the other connections in the room...
     this.party.broadcast(
-      `${sender.id}: ${message}`,
+      JSON.stringify({"new_chat": newMessage})//,
       // ...except for the connection it came from
-      [sender.id]
+      // [sender.id]
     );
   }
+  
 }
 
 Server satisfies Party.Worker;
